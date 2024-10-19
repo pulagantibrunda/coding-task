ceb6ddc02595ee403c760bd9bef29a6062358c8a
pulagantibrunda <159898859+pulagantibrunda@users.noreply.github.com>
2024-10-19 12:36:26 +0530

    Update App.js

--- coding-task/src/App.js
+++ coding-task/src/App.js
@@ -5,7 +5,7 @@ import ThemeToggler from './components/headercomponents/header';
 function App() {
   return (
     <div className="App">
-   // <ThemeToggler/>
+
     <ModeSwitcher/>
     </div>
   );
