0ef38b44dfdf3c7c71849acfef827d2220e07b66
pulagantibrundha <pulagantibrundha2002@gmail.com>
2024-10-19 10:26:23 +0530

    Initial commit

---
+++ coding-task/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import ThemeToggler from './components/headercomponents/header';
+
+function App() {
+  return (
+    <div className="App">
+   <ThemeToggler/>
+    </div>
+  );
+}
+
+export default App;
